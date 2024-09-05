f3a0572c9f0575d09c02136de368032c97c9f8f8
Patrick <patrick-kamdem@patelot.de>
2024-09-05 13:44:55 +0200

    code refactoring

--- src/components/psa/Psa.tsx
+++ src/components/psa/Psa.tsx
@@ -1,5 +1,6 @@
 import classNames from "classnames";
 import { useState } from "react";
+import { FaArrowRight } from "react-icons/fa";
 
 const Psa = () => {
   // state
@@ -20,7 +21,10 @@ const Psa = () => {
     >
       <div className={"relative -top-[3.5rem]"}>
         <div className="absolute top-[1.5rem] h-[2rem] w-[2rem] -right-[.25rem] cursor-pointer z-10">
-          <button className="btn btn-circle btn-outline" onClick={handleClick}>
+          <button
+            className="btn btn-circle btn-outline bg-white hover:bg-[olive]"
+            onClick={handleClick}
+          >
             <svg
               xmlns="http://www.w3.org/2000/svg"
               className="h-6 w-6"
@@ -47,20 +51,22 @@ const Psa = () => {
       </div>
       <div className={"relative -top-[4.5rem]"}>
         <div className="relative py-4 px-14 inline-block text-[1.125rem] font-overpass-bold rounded-[10px] -left-[.5rem] bg-[olive] leading-[.875rem] text-[#f4f4f4]">
-          Patelot.de
+          <span className="font-bold">patelot.de</span>
         </div>
         <div className="p-[1rem] text-white">
-          <div className="text-lg mb-4 font-bold">Patrick Kamdem</div>
-          <div className="text-sm leading-[1.125rem] mb-4">
-            Ich helfe Dir gerne
+          <div className="mb-4 font-bold uppercase text-black">
+            Patrick Kamdem
+          </div>
+          <div className="text-sm leading-[1.125rem] my-4 text-center">
+            Ich helfe Dir gerne.
           </div>
         </div>
-        <div className="py-0">
+        <div>
           <a
             href="https://patelot.de"
-            className="p-[1rem] hover:text-white hover:font-bold"
+            className="p-[1rem] hover:text-white hover:font-bold flex justify-center items-center"
           >
-            Merh erfahren ...
+            <span className="mr-2">Merh erfahren</span> <FaArrowRight />
           </a>
         </div>
       </div>
